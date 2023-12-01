bd1548815df0465d886046d524e4f090b4087d65
mateusz <spiewaak9@gmail.com>
2023-12-01 18:37:54 +0100

    fixed task 2

--- day1/task2.ts
+++ day1/task2.ts
@@ -2,27 +2,32 @@ const inputFile = Bun.file('input.txt');
 const inputs = (await inputFile.text()).split('\n');
 
 const numbersDictionary = {
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9,
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
 };
 
 let sum = 0;
 
 for (let input of inputs) {
-  const n = input
-    .split(/(one|two|three|four|five|six|seven|eight|nine)/g)
-    .map((word: string) => numbersDictionary[word] ?? word)
-    .join()
-    .replace(/[^\d]/g, '');
-  const number = n ? Number(n[0] + n[n.length - 1]) : 0;
-  sum += number;
+  const regex = /(?=(two|one|three|four|five|six|seven|eight|nine|\d))/g;
+
+  let m = [];
+  let x = [];
+
+  while ((m = regex.exec(input)) !== null) {
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    x.push(numbersDictionary[m[1]] ?? m[1]);
+  }
+  sum += x.length > 0 ? Number(x[0] + x[x.length - 1]) : 0;
 }
 
 console.log(sum);
